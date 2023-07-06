8a9078b4099f2b5ac7e942929cd0bb1a75fc6f47
Olegsion <maxloh2007@gmail.com>
2023-07-06 22:17:52 +0600

    userPopup

--- components/Popups/UserPopup.tsx
+++ components/Popups/UserPopup.tsx
@@ -2,8 +2,32 @@ import {FC, memo, useEffect, useState} from 'react'
 import {useAppDispatch, useAppSelector} from "@/store/index";
 import {AnimatePresence} from "framer-motion";
 import PopupBlock from "@/components/Popups/PopupBlock";
+import style from "@/components/Session/Users/Users.module.scss";
+import Image from "next/image";
+import {userDataI, userGameDataI} from "@/store/interfaces/user";
 
-const UserPopup: FC = () => {
+interface UserPopupProps {
+    userData: userDataI
+    userGameData: userGameDataI
+}
+
+
+interface CommonInfoLineProps {
+    title: string
+    value: string
+}
+
+
+const CommonInfoLine: FC<CommonInfoLineProps> = ({title, value}) => {
+    return (
+        <div>
+            <span className={style.info}>{title}</span>
+            <span className={style.info}>{value}</span>
+        </div>
+    )
+}
+
+const UserPopup: FC<UserPopupProps> = props => {
     const currentPopup = useAppSelector(state => state.popups.currentPopup)
     const [isLoading, handleLoading] = useState<boolean>(false)
 
@@ -16,7 +40,39 @@ const UserPopup: FC = () => {
     return (
         <AnimatePresence>
             {currentPopup === 0 && (
-                <PopupBlock title={''}>
+                <PopupBlock style={{background: 'var(--colorBlackB)'}}>
+                        <div className={style.userInfoWrapper}>
+                            <div className={style.avatarWrapper}>
+                                {props.userData?.avatar && props.userGameData && (
+                                    <>
+                                        <Image
+                                            className={style.avatar}
+                                            src={props.userData.avatar}
+                                            alt={'avatar'}
+                                            width={50}
+                                            height={50}
+                                        />
+                                        <div
+                                            className={style.circle}
+                                            style={{ border: `${props.userGameData.color.hex} solid 2px` }}
+                                        />
+                                    </>
+                                )}
+                            </div>
+                            <div className={style.userCommonInfo}>
+                                <div>
+                                    <p className={style.userName}></p>
+                                    <p>|</p>
+                                    <p className={style.userColor}></p>
+                                </div>
+                                <div>
+                                    <p className={style.userEmail}></p>
+                                </div>
+                            </div>
+                            <div className={style.userMoneyInfo}>
+
+                            </div>
+                        </div>
 
                     {/*<Button className={style.button}>*/}
                     {/*	<Image src={githubIcon} alt={'githubIcon'} />*/}
